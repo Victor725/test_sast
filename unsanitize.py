ca094d1ac7630f8857a5b952a416a17a6b403f09
Victor <1149833549@qq.com>
2024-04-24 17:28:40 +0800

    pyre

--- unsanitize.py
+++ unsanitize.py
@@ -1,11 +1,15 @@
 import subprocess
 import os
 
+def unsanitize(command):
+    return "ls"
+    #return command[0:2]
+
 
 def transcode_file():
     Input = input('Please provide the path for the file to transcode: ')
     command = 'ls "{source}" '.format(source=Input)
-    #command = Input
+    command = unsanitize(command)
     os.system(command)  # command exec!
     ping(Input)
 
